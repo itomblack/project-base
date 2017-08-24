c819e4833bf09945fd1f7eac1aee4a00a5e57650
Ian Black <ian@Ian.local>
2017-08-24 15:26:45 -0700

    option 1 design

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
 (function () { 'use strict';
 
-  console.log('test123');
+  $(document).on('focus active', 'input',function(){
+  	$('label[for="'+$(this).attr('id')+'"]').addClass('active');
+  });
+
+  $(document).on('blur', 'input',function(){
+
+	if (!($(this).length && $(this).val().length)) {
+		$('label[for="'+$(this).attr('id')+'"]').removeClass('active');
+	}
+  });
 
 
 
